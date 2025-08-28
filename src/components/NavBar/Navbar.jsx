9ea62a668bf552ee85eb8320861df443e4f6de3b
Joshua <joshua_shepard@yahoo.com>
2025-08-28 15:32:12 -0400

    Create and style nav bar

---
+++ src/components/NavBar/Navbar.jsx
@@ -0,0 +1,22 @@
+import styles from './Navbar.module.css';
+import { motion } from 'framer-motion';
+
+function Navbar() {
+    return (
+        <motion.nav className={styles.navbarContainer}
+            initial={{ y: -300, opacity: 0 }} 
+            animate={{ y: 0, opacity: 1 }} 
+            transition={{ type: "spring", stiffness: 70, damping: 12 }}>
+            
+            <ul className={styles.navbarList}>
+                <motion.li whileHover={{scale: 1.2, transition: {duration: .2 },}} whileTap= {{ scale: 0.9 }}><a href="#home" className={styles.navbarItems}>HOME</a></motion.li>
+                <motion.li whileHover={{scale: 1.2, transition: {duration: .2 },}} whileTap= {{ scale: 0.9 }}><a href="#about" className={styles.navbarItems}>ABOUT</a></motion.li>
+                <motion.li whileHover={{scale: 1.2, transition: {duration: .2 },}} whileTap= {{ scale: 0.9 }}><a href="#projects" className={styles.navbarItems}>PROJECTS</a></motion.li>
+                <motion.li whileHover={{scale: 1.2, transition: {duration: .2 },}} whileTap= {{ scale: 0.9 }}><a href="#contact" className={styles.navbarItems}>CONTACT ME</a></motion.li>
+            </ul>
+            
+        </motion.nav>
+    );
+}
+
+export default Navbar;
