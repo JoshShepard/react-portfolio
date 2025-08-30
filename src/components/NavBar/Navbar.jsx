ded8d95112a0a5c0906c5bcd0e6a78a994a4cf4a
Joshua <joshua_shepard@yahoo.com>
2025-08-29 20:09:12 -0400

    Create Skills component

--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -6,13 +6,13 @@ function Navbar() {
         <motion.nav className={styles.navbarContainer}
             initial={{ y: -300, opacity: 0 }} 
             animate={{ y: 0, opacity: 1 }} 
-            transition={{ type: "spring", stiffness: 70, damping: 12 }}>
+            transition={{ duration: 0.8 }}>
             
             <ul className={styles.navbarList}>
-                <motion.li whileHover={{scale: 1.2, transition: {duration: .2 },}} whileTap= {{ scale: 0.9 }}><a href="#home" className={styles.navbarItems}>HOME</a></motion.li>
-                <motion.li whileHover={{scale: 1.2, transition: {duration: .2 },}} whileTap= {{ scale: 0.9 }}><a href="#skills" className={styles.navbarItems}>SKILLS</a></motion.li>
-                <motion.li whileHover={{scale: 1.2, transition: {duration: .2 },}} whileTap= {{ scale: 0.9 }}><a href="#projects" className={styles.navbarItems}>PROJECTS</a></motion.li>
-                <motion.li whileHover={{scale: 1.2, transition: {duration: .2 },}} whileTap= {{ scale: 0.9 }}><a href="#contact" className={styles.navbarItems}>CONTACT ME</a></motion.li>
+                <motion.li whileHover={{scale: 1.2, transition: {duration: .3 },}} whileTap= {{ scale: 0.9 }}><a href="#home" className={styles.navbarItems}>HOME</a></motion.li>
+                <motion.li whileHover={{scale: 1.2, transition: {duration: .3 },}} whileTap= {{ scale: 0.9 }}><a href="#skills" className={styles.navbarItems}>SKILLS</a></motion.li>
+                <motion.li whileHover={{scale: 1.2, transition: {duration: .3 },}} whileTap= {{ scale: 0.9 }}><a href="#projects" className={styles.navbarItems}>PROJECTS</a></motion.li>
+                <motion.li whileHover={{scale: 1.2, transition: {duration: .3 },}} whileTap= {{ scale: 0.9 }}><a href="#contact" className={styles.navbarItems}>CONTACT ME</a></motion.li>
             </ul>
             
         </motion.nav>
--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,20 +1,87 @@
 import styles from './Skills.module.css';
+import { motion } from 'framer-motion';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faJava, faHtml5, faCss3Alt, faJs, faGit, faReact } from '@fortawesome/free-brands-svg-icons';
+
+// Icon Variant Object
+const skillIconVariant = {
+    hidden: { x: -200, opacity: 0, rotate: 0 },
+    visible: { x: 0, scale: 1, opacity: 1, rotate: 360 },
+};
 
 function Skills() {
     return (
         <section id="skills" className={styles.skillsContainer}>
-            <h2 className={styles.skillTitle}>SKILLS</h2>
-            <div className={styles.skillList}>
-                <FontAwesomeIcon icon={faJava} className={styles.skillIcon} />
-                <FontAwesomeIcon icon={faHtml5} className={styles.skillIcon} />
-                <FontAwesomeIcon icon={faCss3Alt} className={styles.skillIcon} />
-                <FontAwesomeIcon icon={faJs} className={styles.skillIcon} />
-                <FontAwesomeIcon icon={faReact} className={styles.skillIcon} />
-                <FontAwesomeIcon icon={faGit} className={styles.skillIcon} />
-            </div>
-        
+            <motion.h2
+                initial={{ scale: 0, opacity: 0 }}
+                whileInView={{ scale: 1, opacity: 1 }}
+                transition={{ type: 'spring', duration: 1.25, stiffness: 70, damping: 25 }}
+                viewport={{ once:true }}
+                className={styles.skillTitle}>
+                SKILLS
+            </motion.h2>
+
+            <motion.div className={styles.skillList}>
+                <motion.div 
+                    style={{ display: "inline-block"}}
+                    variants={skillIconVariant}
+                    initial="hidden"
+                    whileInView="visible"
+                    transition={{ type: 'spring', duration: 1.5, stiffness: 70, damping: 25, delay: 0.2 }}
+                    viewport={{ once:true }}>
+                    <FontAwesomeIcon icon={faJava} className={styles.skillIcon} />  
+                </motion.div>
+
+                <motion.div 
+                    style={{ display: "inline-block"}}
+                    variants={skillIconVariant}
+                    initial="hidden"
+                    whileInView="visible"
+                    transition={{ type: 'spring', duration: 1.5, stiffness: 70, damping: 25, delay: 0.4 }}
+                    viewport={{ once:true }}>
+                    <FontAwesomeIcon icon={faHtml5} className={styles.skillIcon} /> 
+                </motion.div>
+
+                <motion.div 
+                    style={{ display: "inline-block"}}
+                    variants={skillIconVariant}
+                    initial="hidden"
+                    whileInView="visible"
+                    transition={{ type: 'spring', duration: 1.5, stiffness: 70, damping: 25, delay: 0.6 }}
+                    viewport={{ once:true }}>
+                    <FontAwesomeIcon icon={faCss3Alt} className={styles.skillIcon} />
+                </motion.div>
+
+                <motion.div 
+                    style={{ display: "inline-block"}}
+                    variants={skillIconVariant}
+                    initial="hidden"
+                    whileInView="visible"
+                    transition={{ type: 'spring', duration: 1.5, stiffness: 70, damping: 25, delay: 0.8 }}
+                    viewport={{ once:true }}>
+                    <FontAwesomeIcon icon={faJs} className={styles.skillIcon} />
+                </motion.div>
+
+                <motion.div 
+                    style={{ display: "inline-block"}}
+                    variants={skillIconVariant}
+                    initial="hidden"
+                    whileInView="visible"
+                    transition={{ type: 'spring', duration: 1.5, stiffness: 70, damping: 25, delay: 1.0 }}
+                    viewport={{ once:true }}>
+                    <FontAwesomeIcon icon={faReact} className={styles.skillIcon} />
+                </motion.div>
+
+                <motion.div 
+                    style={{ display: "inline-block"}}
+                    variants={skillIconVariant}
+                    initial="hidden"
+                    whileInView="visible"
+                    transition={{ type: 'spring', duration: 1.5, stiffness: 70, damping: 25, delay: 1.2 }}
+                    viewport={{ once:true }}>
+                    <FontAwesomeIcon icon={faGit} className={styles.skillIcon} />
+                </motion.div>
+            </motion.div>        
         </section>
     );
 }
